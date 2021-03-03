f88ec39161dc33e5ad2360145fc743568e27fb09
Kirsten <kirstenmiller16@gmail.com>
2021-03-03 17:43:10 +0000

    Fixing mistake with last push

--- p4api.net-unit-test/RepositoryTest.Label.cs
+++ p4api.net-unit-test/RepositoryTest.Label.cs
@@ -8,68 +8,68 @@ namespace p4api.net.unit.test
 {
 
 
-	/// <summary>
-	///This is a test class for RepositoryTest and is intended
-	///to contain RepositoryTest Unit Tests
-	///</summary>
-	public partial class RepositoryTest
-	{
-		/// <summary>
-		///A test for CreateLabel
-		///</summary>
-		[TestMethod()]
-		public void CreateLabelTest()
-		{
-			bool unicode = false;
+    /// <summary>
+    ///This is a test class for RepositoryTest and is intended
+    ///to contain RepositoryTest Unit Tests
+    ///</summary>
+    public partial class RepositoryTest
+    {
+        /// <summary>
+        ///A test for CreateLabel
+        ///</summary>
+        [TestMethod()]
+        public void CreateLabelTest()
+        {
+            bool unicode = false;
 
-			string uri = "localhost:6666";
-			string user = "admin";
-			string pass = string.Empty;
-			string ws_client = "admin_space";
+            string uri = "localhost:6666";
+            string user = "admin";
+            string pass = string.Empty;
+            string ws_client = "admin_space";
 
-			for (int i = 0; i < 2; i++) // run once for ascii, once for unicode
-			{
-				Process p4d = new Process();
+            for (int i = 0; i < 2; i++) // run once for ascii, once for unicode
+            {
+                Process p4d = new Process();
 
-				p4d = Utilities.DeployP4TestServer(TestDir, 9, unicode);
+                p4d = Utilities.DeployP4TestServer(TestDir, 9, unicode);
 
-				Server server = new Server(new ServerAddress(uri));
-				try
-				{
-					Repository rep = new Repository(server);
+                Server server = new Server(new ServerAddress(uri));
+                try
+                {
+                    Repository rep = new Repository(server);
 
-					using (Connection con = rep.Connection)
-					{
-						con.UserName = user;
-						con.Client = new Client();
-						con.Client.Name = ws_client;
+                    using (Connection con = rep.Connection)
+                    {
+                        con.UserName = user;
+                        con.Client = new Client();
+                        con.Client.Name = ws_client;
 
-						bool connected = con.Connect(null);
-						Assert.IsTrue(connected);
+                        bool connected = con.Connect(null);
+                        Assert.IsTrue(connected);
 
-						Assert.AreEqual(con.Status, ConnectionStatus.Connected);
+                        Assert.AreEqual(con.Status, ConnectionStatus.Connected);
 
-						Label l = new Label();
-						l.Id = "newLabel";
-						l.Owner = "admin";
-						l.Description = "created by admin";
+                        Label l = new Label();
+                        l.Id = "newLabel";
+                        l.Owner = "admin";
+                        l.Description = "created by admin";
 #pragma warning disable 618
-                  l.Options = "locked";
+                        l.Options = "locked";
 #pragma warning restore 618
-						l.ViewMap = new ViewMap();
-						string v0 = "//depot/main/...";
-						string v1 = "//depot/rel1/...";
-						string v2 = "//depot/rel2/...";
-						string v3 = "//depot/dev/...";
-						l.ViewMap.Add(v0);
-						l.ViewMap.Add(v1);
-						l.ViewMap.Add(v2);
-						l.ViewMap.Add(v3);
+                        l.ViewMap = new ViewMap();
+                        string v0 = "//depot/main/...";
+                        string v1 = "//depot/rel1/...";
+                        string v2 = "//depot/rel2/...";
+                        string v3 = "//depot/dev/...";
+                        l.ViewMap.Add(v0);
+                        l.ViewMap.Add(v1);
+                        l.ViewMap.Add(v2);
+                        l.ViewMap.Add(v3);
 
-						Label newLabel = rep.CreateLabel(l, null);
+                        Label newLabel = rep.CreateLabel(l, null);
 
-						Assert.IsNotNull(newLabel);
-						Assert.AreEqual("newLabel", newLabel.Id);
+                        Assert.IsNotNull(newLabel);
+                        Assert.AreEqual("newLabel", newLabel.Id);
 
                         string v4 = "\"//depot/rel2/a file with spaces\"";
                         newLabel.ViewMap.Add(v4);
@@ -77,7 +77,7 @@ namespace p4api.net.unit.test
                         newLabel = rep.UpdateLabel(newLabel);
 
                         Assert.IsNotNull(newLabel);
-                        Assert.AreEqual(newLabel.ViewMap.Count,5);
+                        Assert.AreEqual(newLabel.ViewMap.Count, 5);
 
                         Label l2 = new Label();
                         l2.Id = "newLabel2";
@@ -87,15 +87,15 @@ namespace p4api.net.unit.test
                         Assert.IsNotNull(newLabel2);
                         Assert.AreEqual(newLabel2.ViewMap.Count, 5);
 
-					}
-				}
-				finally
-				{
-					Utilities.RemoveTestServer(p4d, TestDir);
-				}
-				unicode = !unicode;
-			}
-		}
+                    }
+                }
+                finally
+                {
+                    Utilities.RemoveTestServer(p4d, TestDir);
+                }
+                unicode = !unicode;
+            }
+        }
 
         /// <summary>
         ///A test for UpdateLabelascii
@@ -177,55 +177,55 @@ namespace p4api.net.unit.test
         /// A test for DeleteLabel
         ///</summary>
         [TestMethod()]
-		public void DeleteLabelTest()
-		{
-			bool unicode = false;
+        public void DeleteLabelTest()
+        {
+            bool unicode = false;
 
-			string uri = "localhost:6666";
-			string user = "admin";
-			string pass = string.Empty;
-			string ws_client = "admin_space";
+            string uri = "localhost:6666";
+            string user = "admin";
+            string pass = string.Empty;
+            string ws_client = "admin_space";
 
-			for (int i = 0; i < 2; i++) // run once for ascii, once for unicode
-			{
-				Process p4d = Utilities.DeployP4TestServer(TestDir, 8, unicode);
-				Server server = new Server(new ServerAddress(uri));
-				try
-				{
-					Repository rep = new Repository(server);
+            for (int i = 0; i < 2; i++) // run once for ascii, once for unicode
+            {
+                Process p4d = Utilities.DeployP4TestServer(TestDir, 8, unicode);
+                Server server = new Server(new ServerAddress(uri));
+                try
+                {
+                    Repository rep = new Repository(server);
 
-					using (Connection con = rep.Connection)
-					{
-						con.UserName = user;
-						con.Client = new Client();
-						con.Client.Name = ws_client;
+                    using (Connection con = rep.Connection)
+                    {
+                        con.UserName = user;
+                        con.Client = new Client();
+                        con.Client.Name = ws_client;
 
-						bool connected = con.Connect(null);
-						Assert.IsTrue(connected);
+                        bool connected = con.Connect(null);
+                        Assert.IsTrue(connected);
 
-						Assert.AreEqual(con.Status, ConnectionStatus.Connected);
+                        Assert.AreEqual(con.Status, ConnectionStatus.Connected);
 
-						IList<Label> llist = rep.GetLabels(null);
+                        IList<Label> llist = rep.GetLabels(null);
 
-						Assert.IsNotNull(llist);
+                        Assert.IsNotNull(llist);
 
-						Label deleteTarget = new Label();
-						deleteTarget.Id = "admin_label";
-						rep.DeleteLabel(deleteTarget, null);
+                        Label deleteTarget = new Label();
+                        deleteTarget.Id = "admin_label";
+                        rep.DeleteLabel(deleteTarget, null);
 
-						llist = rep.GetLabels(null);
+                        llist = rep.GetLabels(null);
 
-						Assert.IsNull(llist);
+                        Assert.IsNull(llist);
 
-					}
-				}
-				finally
-				{
-					Utilities.RemoveTestServer(p4d, TestDir);
-				}
-				unicode = !unicode;
-			}
-		}
+                    }
+                }
+                finally
+                {
+                    Utilities.RemoveTestServer(p4d, TestDir);
+                }
+                unicode = !unicode;
+            }
+        }
 
         /// <summary>
         /// A test for LockLabel
@@ -268,7 +268,7 @@ namespace p4api.net.unit.test
                         rep.UpdateLabel(l2);
                         Label l3 = rep.GetLabel("admin_label");
                         Assert.IsFalse(l3.Locked);
-                        
+
                     }
                 }
                 finally
@@ -279,59 +279,59 @@ namespace p4api.net.unit.test
             }
         }
 
-		/// <summary>
-		///A test for GetLabel
-		///</summary>
-		[TestMethod()]
-		public void GetLabelTest()
-		{
-			bool unicode = false;
+        /// <summary>
+        ///A test for GetLabel
+        ///</summary>
+        [TestMethod()]
+        public void GetLabelTest()
+        {
+            bool unicode = false;
 
-			string uri = "localhost:6666";
-			string user = "admin";
-			string pass = string.Empty;
-			string ws_client = "admin_space";
+            string uri = "localhost:6666";
+            string user = "admin";
+            string pass = string.Empty;
+            string ws_client = "admin_space";
 
-			for (int i = 0; i < 2; i++) // run once for ascii, once for unicode
-			{
-				Process p4d = Utilities.DeployP4TestServer(TestDir, 8, unicode);
-				Server server = new Server(new ServerAddress(uri));
-				try
-				{
-					Repository rep = new Repository(server);
+            for (int i = 0; i < 2; i++) // run once for ascii, once for unicode
+            {
+                Process p4d = Utilities.DeployP4TestServer(TestDir, 8, unicode);
+                Server server = new Server(new ServerAddress(uri));
+                try
+                {
+                    Repository rep = new Repository(server);
 
-					using (Connection con = rep.Connection)
-					{
-						con.UserName = user;
-						con.Client = new Client();
-						con.Client.Name = ws_client;
+                    using (Connection con = rep.Connection)
+                    {
+                        con.UserName = user;
+                        con.Client = new Client();
+                        con.Client.Name = ws_client;
 
-						bool connected = con.Connect(null);
-						Assert.IsTrue(connected);
+                        bool connected = con.Connect(null);
+                        Assert.IsTrue(connected);
 
-						Assert.AreEqual(con.Status, ConnectionStatus.Connected);
+                        Assert.AreEqual(con.Status, ConnectionStatus.Connected);
 
-						string targetLabel = "admin_label";
+                        string targetLabel = "admin_label";
 
-						Label l = rep.GetLabel(targetLabel);
+                        Label l = rep.GetLabel(targetLabel);
 
-						Assert.IsNotNull(l);
+                        Assert.IsNotNull(l);
 
-						Assert.AreEqual(targetLabel, l.Id);
+                        Assert.AreEqual(targetLabel, l.Id);
 
-						Assert.AreEqual(l.ViewMap.Count, 1);
-						Assert.IsTrue(l.ViewMap[0].Type.Equals(MapType.Include));
-						Assert.IsTrue(l.ViewMap[0].Left.Path.Equals("//depot/..."));
+                        Assert.AreEqual(l.ViewMap.Count, 1);
+                        Assert.IsTrue(l.ViewMap[0].Type.Equals(MapType.Include));
+                        Assert.IsTrue(l.ViewMap[0].Left.Path.Equals("//depot/..."));
 
-					}
-				}
-				finally
-				{
-					Utilities.RemoveTestServer(p4d, TestDir);
-				}
-				unicode = !unicode;
-			}
-		}
+                    }
+                }
+                finally
+                {
+                    Utilities.RemoveTestServer(p4d, TestDir);
+                }
+                unicode = !unicode;
+            }
+        }
 
         /// <summary>
         ///A test for GetLabelWithRevision
@@ -387,7 +387,7 @@ namespace p4api.net.unit.test
 
                         rep.CreateLabel(l);
 
-                        
+
 
                         l = rep.GetLabel(targetLabel);
 
@@ -410,89 +410,10 @@ namespace p4api.net.unit.test
             }
         }
 
-		/// <summary>
-		///A test for GetLabels
-		///</summary>
-		[TestMethod()]
-		public void GetLabelsTest()
-		{
-			bool unicode = false;
-
-			string uri = "localhost:6666";
-			string user = "admin";
-			string pass = string.Empty;
-			string ws_client = "admin_space";
-
-			for (int i = 0; i < 2; i++) // run once for ascii, once for unicode
-			{
-				Process p4d = Utilities.DeployP4TestServer(TestDir, 8, unicode);
-				Server server = new Server(new ServerAddress(uri));
-				try
-				{
-					Repository rep = new Repository(server);
-
-					using (Connection con = rep.Connection)
-					{
-						con.UserName = user;
-						con.Client = new Client();
-						con.Client.Name = ws_client;
-
-						bool connected = con.Connect(null);
-						Assert.IsTrue(connected);
-
-						Assert.AreEqual(con.Status, ConnectionStatus.Connected);
-                       
- 						Options ops = new Options();
-						ops["-m"] = "1";
-                        IList<Label> l = rep.GetLabels(ops);
-
-
-						Assert.IsNotNull(l);
-						Assert.AreEqual(1, l.Count);
-						Assert.AreEqual("Created by admin.\n", l[0].Description);
-
-                        //now test for options set for a label that does not exist
-                        ops = new Options();
-                        ops["-u"] = "nonexistantuser";
-                        l = rep.GetLabels(ops);
-                        Assert.IsNull(l);
-
-                        // associate a file with a label
-                        FileSpec fs = new FileSpec(new DepotPath("//depot/Modifiers/ReadMe.txt"), null);
-
-                        IList<FileSpec> lfs = new List<FileSpec>();
-                        lfs.Add(fs);
-
-                        Options ops2 = new Options();
-                        IList<FileSpec> target = rep.TagFiles(lfs, "admin_label", ops2);
-                        Assert.IsNotNull(target);
-
-                        // get labels which include files in the path //depot/Modifiers/...
-                        FileSpec path = new FileSpec(new DepotPath("//depot/Modifiers/..."), null);
-                        Options ops3 = new Options();
-                        IList<Label> l2 = rep.GetLabels(ops3, path);
-
-                        Assert.IsNotNull(l2);
-                        Assert.AreEqual(1, l2.Count);
-                        Assert.AreEqual("Created by admin.\n", l2[0].Description);
-
-
-					}
-				}
-				finally
-				{
-					Utilities.RemoveTestServer(p4d, TestDir);
-				}
-				unicode = !unicode;
-			}
-		}
-
-
-
         /// <summary>
-		///A test for GetLabels
-		///</summary>
-		[TestMethod()]
+        ///A test for GetLabels
+        ///</summary>
+        [TestMethod()]
         public void GetLabelsTest()
         {
             bool unicode = false;
@@ -547,7 +468,7 @@ namespace p4api.net.unit.test
                         Assert.IsNotNull(target);
 
                         // get labels which include files in the path //depot/Modifiers/...
-                        FileSpec path = new FileSpec(new DepotPath("//depot/Modifiers@/..."), null);
+                        FileSpec path = new FileSpec(new DepotPath("//depot/Modifiers/..."), null);
                         Options ops3 = new Options();
                         IList<Label> l2 = rep.GetLabels(ops3, path);
 
@@ -601,8 +522,8 @@ namespace p4api.net.unit.test
                         // tests to ensure old labels options work, prior
                         // to ServerID argument
 
-                        Options opts = new Options(LabelsCmdFlags.None,null,null,
-                            50,null);
+                        Options opts = new Options(LabelsCmdFlags.None, null, null,
+                            50, null);
 
                         IList<Label> labels = rep.GetLabels(opts);
 
@@ -662,7 +583,7 @@ namespace p4api.net.unit.test
                         Assert.AreEqual(con.Status, ConnectionStatus.Connected);
 
                         LabelsCmdOptions opts = new LabelsCmdOptions(LabelsCmdFlags.None,
-                           null, null,50,null,null);
+                           null, null, 50, null, null);
                         IList<Label> labels = rep.GetLabels(opts);
                         Assert.AreEqual("Created by admin.\n", labels[0].Description);
 
@@ -722,7 +643,7 @@ namespace p4api.net.unit.test
                         Assert.IsTrue(connected);
                         Assert.AreEqual(con.Status, ConnectionStatus.Connected);
 
-                        LabelCmdOptions opts = new LabelCmdOptions(LabelCmdFlags.Global,null);
+                        LabelCmdOptions opts = new LabelCmdOptions(LabelCmdFlags.Global, null);
 
                         Label label = rep.GetLabel("admin_label", null, opts);
                         Assert.IsNull(opts["-g"]);
@@ -740,5 +661,5 @@ namespace p4api.net.unit.test
                 unicode = !unicode;
             }
         }
-	}
+    }
 }
